e34df8258262c5f4c0f832cb38601ed3a1c3f82a
MasahiroMarkTsujimura <masahiromarktsujimura@gmail.com>
2017-02-28 11:28:13 -0800

    Update JSON.php

--- JSON.php
+++ JSON.php
@@ -1,20 +1,28 @@
 <?php 
 namespace securechat;
 
-class JSONfile {
+class JASONfile {
 var $RSAciphertext
 var $AESciphertext
 var $HMACtag
+
 
 	public function __construct($RSAciphertext, $AESciphertext, $HMACtag) {
 		$this->RSAciphertext = $RSAciphertext;
 		$this->AESciphertext = $AESciphertext;
 		$this->HMACtag= $HMACtag;
-		$json_data = json_encode($this);
+		$json_data = json_encode($this->expose());
+		
 		
 		//Print to JSON file
 		file_put_contents('data.json', $json_data);
 	
 	}
+	
+	public function expose() {
+		return get_object_vars($this);
+	}
+
+
 }
 ?>
